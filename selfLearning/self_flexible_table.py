cdf09f0bf9a3c9f6558a1abb6eb6cfeb9994b282
Ankit <ankit@Ankits-Laptop.local>
2025-05-24 22:11:12 +0545

    fexible_table

---
+++ selfLearning/self_flexible_table.py
@@ -0,0 +1,7 @@
+b = "Title"
+space = 18
+no_of_colums = 2
+
+print("-"*((space * no_of_colums) + (no_of_colums + 1)))
+print("|"+f"{b.center(space)}|"*no_of_colums)
+print("-"*((space * no_of_colums) + (no_of_colums + 1)))
