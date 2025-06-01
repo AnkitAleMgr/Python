da5755391db7ef881303cd13413774f049c2ede4
Ankit <anmolankit00@gmail.com>
2025-06-01 15:36:42 +0000

    file handeling completed

--- selfLearning/file_handeling/main.py
+++ selfLearning/file_handeling/main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+with open("selfLearning/file_handeling/file.txt", "a+") as f:
+  # f.write("Hello World\n")
+  # f.truncate(20)
+
+  # f.seek(2)
+  f.read(5)
+  print(f.tell())
