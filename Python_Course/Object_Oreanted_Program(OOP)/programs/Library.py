3ba418d94d0d8e9138cd9b28b8208e815731ca61
Ankit <anmolankit00@gmail.com>
2025-06-06 10:15:08 +0000

    new folder and file added

---
+++ Python_Course/Object_Oreanted_Program(OOP)/programs/Library.py
@@ -0,0 +1,10 @@
+class Library:
+
+    def __init__(self, no_of_books, *books):
+        self.__no_of_books = no_of_books
+        self.__books = books
+
+
+
+library1 = Library(2 , "Harry porter", "Spider man")
+    
