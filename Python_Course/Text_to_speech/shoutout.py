8af330c4ef37603fcf9f8062dcd8aa5a53f0a841
Ankit <ankit@Ankits-Laptop.local>
2025-06-10 21:46:03 +0545

    file added

--- Python_Course/Text_to_speech/shoutout.py
+++ Python_Course/Text_to_speech/shoutout.py
@@ -1,5 +1,4 @@
 from gtts import gTTS
-from playsound import playsound
 import os
 
 
@@ -10,17 +9,18 @@ def main():
         # forcing user to put valid input
         text = input("Enter text to convert into speech: ")
         if text == "":
-            print("It cannot be emput. Please enter the value again.")
-        elif not text.isalnum():
+            print("It cannot be empty. Please enter the value again.")
+        elif not all(x.isalnum() or x.isspace() for x in text) or not any(x.isalpha() for x in text):
             print("Invalid input please try again.")
         else:
             break
 
+
     # logic section 
     tts = gTTS(text= text, lang= "en", slow=False)
     tts.save("output.mp3")
-    print("Text has been convented into speech as file named \" output.mp3\"")
-    playsound("output.mp3")
+    print("Text has been convented into spe`ech as file named \" output.mp3\"")
+    os.system("afplay output.mp3")
     
 
 if __name__ == "__main__":
