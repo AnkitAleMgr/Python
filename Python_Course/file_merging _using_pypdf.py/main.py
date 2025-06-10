f233064712aab199052cde055b606ab5050547c2
Ankit <anmolankit00@gmail.com>
2025-06-10 06:37:13 +0000

    faf

--- Python_Course/file_merging _using_pypdf.py/main.py
+++ Python_Course/file_merging _using_pypdf.py/main.py
@@ -1,10 +1,11 @@
 from pypdf import PdfMerger
 
 merger = PdfMerger()
-# Add the files in the order you want
+
+# Add PDF files
 merger.append("file1.pdf")
 merger.append("file2.pdf")
 
-# Write out the combined PDF
-merger.write("merged.pdf")
+# Write to a new PDF
+merger.write("merged_output.pdf")
 merger.close()
