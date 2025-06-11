a043316415a885fe31e1da2a338ad0c350131614
Ankit <anmolankit00@gmail.com>
2025-06-11 04:54:13 +0000

    time_module added

---
+++ Python_Course/Time_module/main.py
@@ -0,0 +1,29 @@
+import time
+
+# start = time.time()
+
+# # Code to measure
+# for i in range(1000000):
+#     pass
+
+# end = time.time()
+
+# print(f"Time taken: {end - start} seconds")
+
+# t = time.localtime()
+# format = time.strftime("%Y-%m-%d-%H-%M-%S")
+# print(format)
+
+# print(dir(time))
+# print(time.time_ns())
+# print(time.perf_counter())
+# print(time.perf_counter())
+# print(time.perf_counter())
+# print(time.process_time())
+# print("hello")
+# print(time.process_time())
+# format = time.strftime("%Y-%m-%d-%H-%M-%s")
+# format = time.localtime()
+print(time.asctime(time.localtime()))
+print(time.ctime(time.time()))
+# print()
