d30fd65590fbd8a5a8fe14eb606cafed12f0a8c5
Ankit <anmolankit00@gmail.com>
2025-06-12 15:57:29 +0000

    comment has been added in simple_cli.py

--- Python_Course/command_line_utility/simple_cli.py
+++ Python_Course/command_line_utility/simple_cli.py
@@ -1,11 +1,13 @@
-import argparse
+import argparse # importing module name argsparse for CLI(Command Line Interface)
 
+# Initilizing the ArgumentParer
 parser = argparse.ArgumentParser(
+    # giving discription and proper input structure
     description="A simple calculator that compute : x² + y² of any two user input.",
     usage = "python <path/filename.py> <first number> <second number> [-v | -vv]"
 )
 
-
+# adding arguments
 parser.add_argument("first", help="First number", type= int)
 parser.add_argument("second", help="second number", type= int)
 parser.add_argument(
@@ -15,9 +17,11 @@ parser.add_argument(
     default=0,
 )
 
+# parsing arguments and computing output
 args = parser.parse_args()
 output = args.first ** 2 + args.second ** 2
 
+# logical section/ output section
 match args.verbose:
     case 1:
         print(f"{args.first}² + {args.second}² = {output}")
