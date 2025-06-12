802ea292152389056f5349e5a0bbbb2c5597b0b2
Ankit <anmolankit00@gmail.com>
2025-06-12 15:51:17 +0000

    simple_cli.py complete

---
+++ Python_Course/command_line_utility/simple_cli.py
@@ -0,0 +1,27 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="A simple calculator that compute : x² + y² of any two user input.",
+    usage = "python <path/filename.py> <first number> <second number> [-v | -vv]"
+)
+
+
+parser.add_argument("first", help="First number", type= int)
+parser.add_argument("second", help="second number", type= int)
+parser.add_argument(
+    "-v", "--verbose",
+    help="provide more detail description. Use -v for a summary or -vv for detailed output.",
+    action="count",
+    default=0,
+)
+
+args = parser.parse_args()
+output = args.first ** 2 + args.second ** 2
+
+match args.verbose:
+    case 1:
+        print(f"{args.first}² + {args.second}² = {output}")
+    case 2:
+        print(f"First number: {args.first}\nSecond number: {args.second}\nFormula: x² + y²\noutput: {output}")
+    case _:
+        print(output)
--- Python_Course/command_line_utility/square.py
+++ Python_Course/command_line_utility/square.py
@@ -1,13 +1,18 @@
 import argparse
 
-parser = argparse.ArgumentParser(description="Square of number.")
+parser = argparse.ArgumentParser(description="Square of one digit number.")
 
-parser.add_argument("-s", "--square", help="square of given number", type= int, required= True, choices=[0,1,2,3,4,5,6,7,8,9])
-parser.add_argument("-v", "--verbose", help="give more detail", action="store_true")
+parser.add_argument("-s", "--square", help="square of given number", type= int, choices=[0,1,2,3,4,5,6,7,8,9], default= 5, nargs = "?") #required = True
+parser.add_argument("-v", "--verbose", help="give more detail", action="count", required = True, )
 
 args = parser.parse_args()
 
-if args.verbose:
+if args.verbose == 0:
+    print(args.square ** 2)
+elif args.verbose == 1:
     print(f"{args.square} square is {args.square**2}")
+elif args.verbose == 2:
+    print(f"{args.square} square is : {args.square**2}")
 else:
-    print(args.square**2)
+    print("Invalid varbose")
+    
