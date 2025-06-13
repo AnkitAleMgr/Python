bb31127a704a55f1e4d82597cae85536d9104b8e
Ankit <anmolankit00@gmail.com>
2025-06-13 10:32:50 +0000

    comment added in running_sum_calculatory.py

--- Python_Course/walrus_operator/running_sum_calculator.py
+++ Python_Course/walrus_operator/running_sum_calculator.py
@@ -2,7 +2,15 @@
 # Assign the input to a variable
 # Calculate the running sum while checking if it's still under 100
 
-total = 0
+def main():
+    # variable
+    total:int = 0
 
-while ( total := (value := int(input("Enter a number: ")) + total)) < 99 :
-    print(total)
+    # checking if it is below 100 or not
+    while ( total := (value := int(input("Enter a number: ")) + total)) < 99 :
+        print(total)
+        
+
+# entry point
+if __name__ == "__main__":
+    main()
