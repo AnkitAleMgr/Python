c699ba3d636f556893d7e80d4034edae25089c3b
Ankit <anmolankit00@gmail.com>
2025-06-13 09:15:12 +0000

    food.py complete

--- Python_Course/walrus_operator/food.py
+++ Python_Course/walrus_operator/food.py
@@ -1,3 +1,10 @@
+import os
+
 foods = list()
 
-print(foods)
+while (food := input("Enter a food: ")) != "exit":
+    foods.append(food)
+    print(f"{food} has been add in {foods}")
+
+os.system("clear")
+print(f'{(str_food := ", ".join(foods))}')
