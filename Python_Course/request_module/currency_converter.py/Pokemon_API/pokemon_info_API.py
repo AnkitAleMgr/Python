ff4f10fe33cab41704ea71a8b4662f990eaf92fe
Ankit <ankit@Ankits-Laptop.local>
2025-06-20 23:23:10 +0545

    75% completer of pokemon_info _api.py

--- Python_Course/request_module/currency_converter.py/Pokemon_API/pokemon_info_API.py
+++ Python_Course/request_module/currency_converter.py/Pokemon_API/pokemon_info_API.py
@@ -1,6 +1,5 @@
-from curses.ascii import isalnum
-from http.client import responses
-import requests, os
+from urllib import response
+import requests, os, difflib
 from dotenv import load_dotenv
 
 # for acessing .env for data and url
@@ -10,9 +9,9 @@ BASE_URL = os.getenv("base_url") #<-- use to get url from .env file using os
 # variable
 EXIT_KEYWORD = {"q","quit","exit","e"} # <-- set of key word used to quit
 
-available_pokemon = requests.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=10")
-available_pokemon_data = available_pokemon.json()
-available_pokemon = [pokemon["name"] for pokemon in available_pokemon_data["results"]]
+# available_pokemon = requests.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=10")
+# available_pokemon_data = available_pokemon.json()
+# available_pokemon = [pokemon["name"] for pokemon in available_pokemon_data["results"]]
 
 
 # user dependent variable
@@ -36,63 +35,86 @@ def user_input(prompt):
 def endpoint_asker():
 
     # limiting the available option
-    Available_end_point = {"Pokemon","Berry","Ability"}
+    Available_end_point = {point for point in requests.get(BASE_URL).json()}
 
     # infinite loop to ask valid end point form the user
     while True:
         # asking input form the user
-        end_point = user_input("\nEnter the Topic you want to see(Pokemon, berry, ability): ")
+        end_point = user_input("\nEnter the Topic you want to see(Pokemon, berry, ability, move): ")
+        suggestion = difflib.get_close_matches(end_point, list(Available_end_point), n=1, cutoff=0.6)
+        
         # checking if it is empty or not
-        if end_point == "" or any(char.isdigit() for char in end_point) or end_point not in Available_end_point:
+        if end_point == "" or any(char.isdigit() for char in end_point):
             print("Invalid input")
         else:
-            print(f"End point {end_point} accepted. Proceding..")
-            return end_point
+            if end_point.lower() in Available_end_point:
+                print(f"\nEnd point {end_point} accepted. Proceding....")
+                print(BASE_URL + "/"+ end_point.lower()+ "\n")
+                return end_point.lower()
+            else:
+                suggestion_text = ", ".join(s.title() for s in suggestion)
+                print (f"Did not found end point named {end_point}, do you mean {", ".join(suggestion).title() + "."}") \
+                    if suggestion else print (f"Did not found end point named {end_point}{", ".title().join(suggestion) + "."}")
 
 # for filter unecessery data by asking user name or ID
 def name_id_asker(end_point):
 
+    url = BASE_URL+"/"+end_point.lower() 
+    response = requests.get(url)
+    data = response.json()
+    total_count = data["count"]
+    print(f"total_count = {total_count}")
+    new_url = BASE_URL+"/"+end_point.lower()+ f"?offset=0&limit={total_count}"
+
+    available_name_id = requests.get(new_url)
+    data = available_name_id.json()
+    result = data["results"]
+    name_list= [pokemon["name"] for pokemon in result]    
+    name_list= {name.replace("-"," ") for name in name_list}
+    
     # infinite loop to ask valid name form the user
     while True:
-        
         # asking name or id from the user 
-        name_id = user_input(f"\nEnter the name or id of {end_point}: ")
+        name_id = user_input(f"\nEnter the name of {end_point}: ")
+        suggestion = difflib.get_close_matches(name_id, list(name_list), n=1, cutoff=0.6)
+
         if name_id == "":
             print("Invalid input")
         else:
-            print(f"Name(ID) {name_id} has been accepted of {end_point}\n")
-            return name_id
+            if name_id.lower() in name_list:
+                print(f"\nName {name_id} accepted as end point of {end_point}")
+                print(BASE_URL+"/"+end_point+"/"+name_or_id)
+                return name_id.lower()
+            print(f"Cannot find {end_point} named {name_id}, Do you mean {"".join(suggestion)}?") \
+                if suggestion else print(f"Cannot find {end_point} named {name_id}, No suggestion.")
+            
 
 
 # function that retrive data form server
-def retriver(id, type):
-    # variable
-    url = "{}/{}/{}".format(BASE_URL, type, id)
-    print(url)
+def detail_retriver(end_point, name):
+    # basic retriving data form server
+    url = BASE_URL+"/"+end_point+"/"+name
+    response = requests.get(url)
+    data = response.json()
+    title = [topic for topic in data]
+    print(f"\n{title}\n{url}")
 
-    responses = requests.get(url)
-    
-    # checking if responce is ok or not
-    if responses.ok:
-        data = responses.json()
-        return data["abilities"]
-    else:
-        print(f"Did not got the data. Error {responses.status_code}")
+    # trying to print each data
+    for index, i in enumerate(title, start=0):
+        print(index, i)
+        print(i)
+        for inner in data[i]:
+            print(inner)
+            print()
+        
+
 
 
 def main():
-    end_point = endpoint_asker()
-    name_or_id = name_id_asker(end_point)
-    data = retriver(name_or_id, end_point)
-    print(data)
-
-    # endpoint_and_name_id_asker()
-
-    # for index, pokemon in enumerate(available_pokemon_data["results"], start=1): 
-    #     print(f"{index:^5}", pokemon["name"])
+    # end_point = endpoint_asker()
+    # name = name_id_asker(end_point)
+    detail_retriver(end_point="pokemon", name="pikachu")
     
-    # print(available_pokemon)
-
 
 
 if __name__ == "__main__":
