6c231ed509db94ec9701bbe89f34b59968059d6c
Ankit <anmolankit00@gmail.com>
2025-07-08 09:28:50 +0545

    new upadte

--- Python_Course/request_module/weather_api/weather_api.py
+++ Python_Course/request_module/weather_api/weather_api.py
@@ -1,5 +1,6 @@
-import os, requests, re
+import os, requests
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv() # <-- creating object to read .env file
 
@@ -29,32 +30,36 @@ def location_input_empty_checker(value):
         user_location()
     return value
 def input_empty_checker(value, function):
-    while value == "":
+    if value == "":
         print("Input cannot be empty. Please re-enter value: ")
-        function
+        return function()
     return value
-def input_boolean_checker(value, fucntion):
-    while value.lower() not in {"yes","no"}:
+def input_boolean_checker(value, function):
+    if value in ["yes","no"]:
+        print(f"{value.capitalize()} is accepted as value.")
+        return value
+    else:
         print("Input can only be in boolean(Yes/No) format. Please re-enter value:")
-        fucntion
-    return value.lower()
+        return function()
+    
 def input_day_range_checker(value, function, first_day, last_day):
     try:
         value = int(value)
     except ValueError:
         print(f"Invalid day. {value} cannot be converted int true number. Please re-enter the day:")
-        function
+        function()
         
-    while not (1 <= value <= 14):
+    while not (first_day <= value <= last_day):
         print(f"Invalid day. It must be between ({first_day} - {last_day}). Please re-enter the day:")
-        function
+        function()
     return value
-def input_date_format_checker(date, function, format):
-    
-    if not re.match(format, date):
-        print()
-    return date
-    
+def input_date_format_checker(date, function):
+    try:
+        valid_date = datetime.strptime(date, "%Y-%m-%d")
+        return valid_date
+    except:
+        print("Invalid date format or non-date input. Please re-enter.")
+        function()
 
 # user dependent fucntion
 def user_endpoint():# <--to get endpoint from available end point
@@ -67,7 +72,6 @@ def user_endpoint():# <--to get endpoint from available end point
     "future": "/future.json",
     "marine": "/marine.json"
     }
-
     line()
     print(f"Available end point ({len(available)}): ")
     line()
@@ -77,14 +81,14 @@ def user_endpoint():# <--to get endpoint from available end point
     while user_end_point not in available.keys():  
         print("Invalid end point. Please re-enter end point..")
         user_end_point = custom_input("Please enter the end point: ")
-    
-    print(f"{user_end_point} accepted as valid endpoint.")
+    print(f"{user_end_point.capitalize()} accepted as valid endpoint.")
+
     return user_end_point, available[user_end_point]
 def user_location():# <--to get location detail from user   
     location = custom_input("Please enter the location: ")
     location = location_input_empty_checker(location)
 
-    print(f"{location} has been accepted as valid location.")
+    print(f"{location.capitalize()} has been accepted as valid location.")
     return location
 def user_params():# <--to get necessery params from user
     
@@ -93,28 +97,36 @@ def user_params():# <--to get necessery params from user
 
 # asking data for prams
 def api():
-    api = custom_input("Do you want api(get air quality data(Yes/No)): ")
-    api = input_empty_checker(api, api())
-    api = input_boolean_checker(api, api()) 
-    return api
-def days():
+    # function = "api"
+    user_api = custom_input("Do you want api(get air quality data(Yes/No)): ")
+    user_api = input_empty_checker(user_api, api)
+    user_api = input_boolean_checker(user_api, api) 
+    return user_api
+def days(last_day):
     first_day = 1
-    last_day = 14
-    days = custom_input("Enter the day duration (1-14): ")
-    days = input_empty_checker(days, days())
-    days = input_day_range_checker(days, days(), first_day, last_day)
-    return days
+    user_days = custom_input(f"Enter the day duration ({first_day}-{last_day}): ")
+    user_days = input_empty_checker(user_days, days)
+    user_days = input_day_range_checker(user_days, days, first_day, int(last_day))
+    return user_days
 def dt():
-    date_format = r"^\d{4}-\d{2}-\d{2}$"
-    date = custom_input("Enter the date (yyyy-MM-dd) format: ") 
-    date = input_empty_checker(date, dt())
-    input_date_format_checker(date, dt(), format)
+    # date_format = r"^\d{4}-\d{2}-\d{2}$"
+    user_date = custom_input("Enter the date (yyyy-MM-dd) format: ") 
+    user_date = input_empty_checker(user_date, dt)
+    user_date = input_date_format_checker(user_date, dt)
+    return user_date
+def alerts():
+    user_alerts = custom_input("Enter if you want alerts(Yes/No): ")
+    user_alerts = input_empty_checker(user_alerts, alerts)
+    user_alerts = input_boolean_checker(user_alerts, alerts)
+    return user_alerts
 
 # handeling params according to user end point choice:
 def current_weather_params():
-    pass
+    user_api = api()
+    return user_api
 def forecast_params():
-    pass
+    user_api = api()
+    print(f"{user_api} form forcase")
 def history_params():
     pass
 def alerts_params():
@@ -129,7 +141,7 @@ def data_retriver(): # <-- requesting data form the server
     location = user_location()
     
     if user_end_point == "current weather":
-        current_weather_params()
+        user_api = current_weather_params()
     elif user_end_point == "forecast":
         forecast_params()
     elif user_end_point == "history":
@@ -148,7 +160,6 @@ def data_retriver(): # <-- requesting data form the server
         pass
     else:
         print(f"Something went wrong {response.status_code}")
-
 
 # main section
 def main():
