0dfa000dc42e9361386de12a1a849e9d610f3c41
Ankit <anmolankit00@gmail.com>
2025-08-09 20:09:36 +0545

    library management system logic section finsihed

---
+++ selfLearning/object_oreanted_program(OOP)/library_management_system/index.py
@@ -0,0 +1,68 @@
+class Book:
+
+    def __init__(self, title : str , author : str, isbn : str , available : bool = True) -> None:
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.available = available
+
+    def display_info(self) -> None:
+        print("Title: ", self.title)
+        print("Author: ", self.author)
+        print("Isbn: ", self.isbn)
+        print("Available: ", self.available)
+
+class Member:
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.borrowed_book = []
+
+
+    def borrow_book(self, book : Book) -> None:
+        if book.available:
+            self.borrowed_book.append(book)
+            book.available = False
+            print(f"{book.title} has been borrowed by {self.name}")
+        else:
+            print(f"{book.title} is not availabe")
+
+    def return_book(self, book : Book) -> None:
+        if book not in self.borrow_book:
+            print(f"There is not book name {book.title} to return.")   
+            return
+        self.borrowed_book.remove(book)
+        book.available = True
+        print(f"{book.title} has been return.")
+
+    def view_borrowed_book(self) -> None:
+        if not self.borrow_book:
+            print("No book has been borrowed")
+            return
+        print("********** Borrowed Book **********")
+        for index, book in enumerate(self.borrowed_book, start= 1):
+            print(f"{index}) {book.title} {book.isbn}")
+        print("***********************************")
+
+class Library:
+
+    def __init__(self,name : str) -> None:
+        self.name = name
+        self.books = []
+
+    def add_book(self, book : Book) -> None:
+        self.books.append(book)
+    
+    def list_available_book(self) -> None:
+        if not self.books:
+            print("No book has been added till now")
+            return
+        print("********** Available Book **********")
+        for book in self.books:
+            if book.available:
+                print("Book")
+        print("********** Available Book **********")
+
+# main entry point
+if __name__ == "__main__":
+    pass
