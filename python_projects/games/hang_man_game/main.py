e31a8d968b8cdd2101337907b124fae88e15d864
Ankit <anmolankit00@gmail.com>
2025-10-22 19:36:31 +0545

    few changes

--- python_projects/games/hang_man_game/main.py
+++ python_projects/games/hang_man_game/main.py
@@ -1,5 +1,10 @@
+from curses.ascii import isdigit
 import requests, os
 
+line_length = 140
+
+def line():
+    print("-" * line_length)
 def custom_input(promp):
     EXIT_WORD = ("e","exit","q","quit")
     ans = input(promp).lower().strip()
@@ -13,28 +18,53 @@ def show_create_profile():
     user_data_dir = current_dir + "/test_folder" # + "/user_data"
     profile_list = list()
 
-    # essential profile
+    # region essential profile(unused)
     def guest_profile():
         pass
     def create_new_profile():
         pass
+    # endregion
 
-    # getting the name of profile names and adding essential profiles
+    # region getting the name of profile and adding essential profiles
     if os.listdir(user_data_dir):    
         profile_list = os.listdir(user_data_dir) 
     profile_list.append("new")
     profile_list.insert(0, "guest")
+    #  endregion
 
-    # breaking long linst in chunks of 3
-    new_profile_list = [profile_list[i:i + 3]  for i in range(0, len(profile_list), 3)]
-    # region displaying the list:
-    for row in new_profile_list:
+    # region displaying and asking profiles
+    line()
+    print("Available Profiles:")
+    line()
+    for index,profile in enumerate(profile_list, start= 1):
+        print(f"    {index}) {profile.capitalize()}")
+    line()
+    while True:
+        try:
+            selected_profile = int(custom_input(f"Choose profile you wnat to use (1-{len(profile_list)}): ").strip())
+            if 1 <= selected_profile <= len(profile_list):
+                break
+        except ValueError:
+            print("Invalid input format.")
         
+
+    line()
+    # endregion
+
+    # region unreletaed (unused)
+    # # breaking long linst in chunks of 3
+    # new_profile_list = [profile_list[i:i + 3]  for i in range(0, len(profile_list), 3)]
+    # # region displaying the list:
+    # for row in new_profile_list:
+    #     print(row)
+    # endregion
+
+    # region identifing selected profile from user
+    
     # endregion
 
 
 
-    
 
 def main():
     show_create_profile()
